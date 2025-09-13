594b4e657db93c18ed1eb68622ad0a8df45fbdb4
traguest <halconvoladorfenix@gmail.com>
2025-09-12 22:19:16 -0600

    creacion del segunda clase

---
+++ scripts/FormValidator.js
@@ -0,0 +1,43 @@
+
+
+const configuracion={
+
+add:".add",
+contenedor:".add__container",
+form:".form",
+h3:"add__title",
+cerrar:"add__close",
+inputTextId:"#text-input",
+claseText:"add__text",
+ContenidoCentrado:"add__content",
+claseEspan:"add__text-input-error",
+tipoEspan:"text-input-error",
+profesionInputId:"#profesion-input",
+tipoEspanProfesion:"profesion-input-error",   
+botonGuardar:"add__save"          
+}
+
+
+
+
+export class FormValidator {
+
+
+constructor(){
+
+
+
+    }
+
+
+
+ _generateTemplate() {
+
+    
+  const getTemplate=  
+
+
+
+}
+
+}
